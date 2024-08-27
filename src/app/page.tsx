e8737679366eb2785ab0965f4e9f10ae1b7b2413
audgeviolin07 <audgeviolin07@gmail.com>
2024-08-26 23:48:30 -0400

    IT FRICKIN WORKS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { Landing } from "@/components/main/landing";
+import ImageClassifier from "@/components/ui/imageClassifier";  // Ensure the correct path
 
 export default async function Home() {
-
   const supabase = createClient();
   const { data: { user } } = await supabase.auth.getUser();
 
-  const userName = user?.user_metadata.full_name || user?.email; 
+  const userName = user?.user_metadata.full_name || user?.email;
 
   console.log(JSON.stringify(user, null, 2));
 
-
   return (
-    <Landing userName={userName}/>
+    <>
+      <Landing userName={userName} />
+      <ImageClassifier />
+    </>
   );
 }
